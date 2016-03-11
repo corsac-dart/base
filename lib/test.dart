1149b7065d5f1955d73a34ea96218437dc1116f9
Anatoly Pulyaevskiy <anatoly@sparkcentral.com>
2016-03-11 15:23:48 -0800

    Removed fixtures from test module

--- lib/test.dart
+++ lib/test.dart
@@ -10,25 +10,12 @@ import 'package:corsac_stateless/di.dart';
 part 'src/test/test_tools.dart';
 
 class TestKernelModule extends KernelModule {
-  final Iterable<Object> fixtures;
-
-  TestKernelModule(this.fixtures);
-
   @override
   Future initialize(Kernel kernel) {
     if (kernel.environment == 'test') {
       // Register in-memory implementations for repository layer.
       kernel.container.addMiddleware(new InMemoryRepositoryDIMiddleware());
     }
-
-    return loadFixtures(kernel);
-  }
-
-  Future loadFixtures(Kernel kernel) {
-    return kernel.execute(() {
-      for (var obj in fixtures) {
-        //
-      }
-    });
+    return new Future.value();
   }
 }
